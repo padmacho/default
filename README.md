15430972a0c3611908202d00c397944735eda159
Padma <padma.chopparapu@gmail.com>
2017-01-24 22:02:55 +0530

    initial

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # default
 Default repo contais website artifact
+# Deploy the change to google app engine
+    gcloud source repos clone default --project=eduami-150711
+Above command will clone the repo that is backing up the app.
+Go to cloned folder and run below command to deploy
+
+    gcloud app deploy
+If the app deployment is sucessful. You can see changes in www.eduami.org
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+markdown  /home/mario/git/hellorest/Rest-Java-SpringBoot.md > /home/mario/git/default/www/tutorials/java/rest/Rest-Java-SpringBoot.html
+#Append at begining of the file
+sed -i '1i\'"<head><link rel='stylesheet' href='http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css'><script src='http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js'></script><script>hljs.initHighlightingOnLoad();</script></head>" /home/mario/git/default/www/tutorials/java/rest/Rest-Java-SpringBoot.html
+
